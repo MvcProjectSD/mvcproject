d44da69e87c3da3d7b227a2d6fa0cffffd805185
MoahmedMagdy <mvcproject6@gmail.com>
2016-04-01 18:51:55 +0200

    CreateAdmin update 3

--- MVC/Controllers/MainAdminController.cs
+++ MVC/Controllers/MainAdminController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using MVC;
+using System.IO;
 
 namespace MVC.Controllers
 {
@@ -43,33 +44,55 @@ namespace MVC.Controllers
         }
 
         [HttpPost]
-        public ActionResult CreateAdmin( string _username , string _password  , string _address, string _phone , double _salary ,int ID ,  string _firstname, string _lastname, string _middlename , DateTime _hiredate , DateTime _birthdate , byte [] _image)
+        public ActionResult CreateAdmin(HttpPostedFileBase file , string _username , string _password  , string _address, string _phone , decimal? _salary , string _firstname, string _lastname, string _middlename , DateTime? _hiredate , DateTime? _birthdate )
         {
+            byte[] array = new byte [2800];
+
+            if (file != null)
+            {
+                string pic = System.IO.Path.GetFileName(file.FileName);
+                string path = System.IO.Path.Combine(
+                                       Server.MapPath("~/images/Users"), pic);
+                // file is uploaded
+                file.SaveAs(path);
+
+                // save the image path path to the database or you can send image 
+                // directly to database
+                // in-case if you want to store byte[] ie. for DB
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    file.InputStream.CopyTo(ms);
+                    array = ms.GetBuffer();
+                }
+
+            }
+
             User user = new User()
             {
                  firstname = _firstname,
                  lastname =  _lastname,
                  middlename = _middlename,
-                 User_ID = ID,
                  hiredate = _hiredate,
-                 birthdate =_birthdate,
-                 Image = _image,
+                 birthdate = (DateTime)_birthdate,
+                 Image = array,
                  phoneNo = _phone,
-                 salary = _salary,
+                 salary = double.Parse( _salary.ToString()),
                  employeeType = 2,
                  address = _address,
             };
 
+            db.Users.Add(user);
+
             Login login = new Login() 
             {
-                 User_ID = ID,
+                 User_ID = user.User_ID,
                  Login_No = 0,
                  UserName = _username,
                  Password = _password ,
                  Type = 2
             };
 
-            db.Users.Add(user);
+            
             db.Logins.Add(login);
 
             db.SaveChanges();
