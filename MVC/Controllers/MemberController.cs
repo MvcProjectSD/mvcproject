95c82ffbe727645f2095546fb62d202204642a79
ayasaeed <as_aya_said@yahoo.com>
2016-04-03 13:13:58 +0200

    MemberHome

---
+++ MVC/Controllers/MemberController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class MemberController : Controller
+    {
+        //
+        // GET: /Member/
+        MVCProjectEntities DB = new MVCProjectEntities();
+        public ActionResult Index()
+        {
+
+            var memberlogin = from item in DB.Members 
+                              join item2 in DB.Logins
+                              on  item.Member_ID equals item2.User_ID 
+                                  where item2.UserName == (String)Session["username"]
+                              select item;
+            return View(memberlogin.ToList());
+        }
+        public ActionResult UpdateProfile()
+        {
+            return View();
+        }
+        public ActionResult MemberHome()
+        {
+            return View();
+        }
+	}
+}
